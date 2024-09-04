b6b570b7ad840c4b90637dd00b3dd6b9e1c18d9d
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-04 22:53:34 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,47 @@
-
-import React, { useEffect, useMemo, useState } from "react";
-import './../styles/App.css';
-
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [allData, setData] = useState([]);
-  const apiData=useMemo(()=>{
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res)=>res.json())
-    .then((data)=>{
-      // console.log(...data);
-      return setData((prev)=>[...prev, ...data])
-    });
-  },[])
- 
-    apiData;
-    
-  
-  return (
-    <ul>
-       {allData.map((data, index)=>{
-        console.log(data)
-           return data ? ( <li key={index}>
-           <h2>{data.title}</h2>
-           <p>{data.body}</p>
-          </li>) : (<p>Loading...</p>) 
-       })}
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [input, setInput] = useState("");
 
-    </ul>
+  useEffect(() => {
+    const fetchData = () => {
+      setLoading(true);
+      try {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+          .then((res) => res.json())
+          .then((data) => setPost(data));
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData()
+  }, []); // Run the effect only once when the component mounts
+
+  return (
+    <div>
+      <div>
+        <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter input to refetch data'/>
+      </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {
+            post.map((p) => (
+              <li key={p.id}>
+                <h4>{p.title}</h4>
+                <p>{p.body}</p>
+              </li>
+            ))
+          }
+        </ul>
+      )}
+    </div>
   )
 }
 
-export default App
+export default App;
