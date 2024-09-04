859461f6ef6b872fa276f0b8dc1c278101231974
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-04 21:33:09 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [allData, setData] = useState([]);
+  const apiData=useMemo(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=>res.json())
+    .then((data)=>{
+      // console.log(...data);
+      return setData((prev)=>[...prev, ...data])
+    });
+  },[])
+ 
+    apiData;
+    
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <ul>
+       {allData.map((data, index)=>{
+        console.log(data)
+           return <li key={index}>
+             <h2>{data.title}</h2>
+             <p>{data.body}</p>
+            </li>
+       })}
+
+    </ul>
   )
 }
 
